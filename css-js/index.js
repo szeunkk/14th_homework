496d8bbaa4a88a485e20afeced331c9f79197369
Eunkyoung Kim <ekkkr@Eunkyoungui-MacBookPro.local>
2025-08-07 19:49:39 +0900

    [250807] 일기보관함_메인 프레임 설정

---
+++ css-js/index.js
@@ -0,0 +1,20 @@
+/* 
+.클래스명 {
+    width: ;
+    height: ;
+    border: ;
+    border-radius: ;
+    padding: ;
+    background-color: ;
+
+
+    display: ;
+    flex-direction: ;
+    justify-content: ;
+    align-items: ;
+    gap: ;
+
+}
+
+
+*/
