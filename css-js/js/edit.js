7e054a9c9a9bf42a8ad69a5d424c5efac28f5365
Eunkyoung Kim <ekkkr@Eunkyoungui-MacBookPro.local>
2025-08-11 19:43:05 +0900

    감정일기장 수정페이지 레이아웃 완성

---
+++ css-js/js/edit.js
@@ -0,0 +1,26 @@
+/* 
+.클래스명 {
+    width: ;
+    height: ;
+    border: ;
+    border-radius: ;
+    outline: none;
+    padding: ;
+    background-color: ;
+
+
+    display: ;
+    flex-direction: ;
+    justify-content: ;
+    align-items: ;
+    gap: ;
+
+    font-weight: 800;
+    font-size: 40px;
+    line-height: 56px;
+    letter-spacing: ;
+    text-align: ;
+    color: ;
+
+}
+*/
