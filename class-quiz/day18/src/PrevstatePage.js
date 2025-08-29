45d549054889d67f58dec59426e672086fd5ec1d
szeunkk <szeunkk@gmail.com>
2025-08-29 22:24:19 +0900

    [트립트립] 리액트 CRA에서 넥스트 CNA로 마이그레이션

--- class-quiz/day18/src/PrevstatePage.js
+++ class-quiz/day18/src/PrevstatePage.js
@@ -5,10 +5,18 @@ export default function PrevstatePage() {
 
   // 함수식을 사용하면 setState가 호출될 때 가장 최신 상태 값을 인자로 받아 비동기적으로 작동하는 문제를 해결 가능
   function sumAll() {
-    setState(state + 1);
-    setState(state => `${state} + 2`); // 함수를 전달하면 함수를 호출할 때 '이전 상태 값'을 인자로 넣어줌
-    setState(state => `${state} + 3`);
-    setState(state => `${state} + 4`);
+     // 함수를 전달하면 함수를 호출할 때 '이전 상태 값'을 인자로 넣어줌
+    // setState(state + 1);
+    // setState(state => `${state} + 2`);
+    // setState(state => `${state} + 3`);
+    // setState(state => `${state} + 4`);
+
+
+
+    setState(state => state + 1); 
+    setState(state => state + 2);
+    setState(state => state + 3);
+    setState(state => state + 4);
   }
 
   return (
