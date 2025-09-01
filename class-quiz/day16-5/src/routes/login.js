33612146aef4e63c8a930d98978cadfedd79d3d6
szeunkk <szeunkk@gmail.com>
2025-09-01 10:08:58 +0900

    [잇츠로드] 이메일, 비밀번호 유효성 검증 추가

--- class-quiz/day16-5/src/routes/login.js
+++ class-quiz/day16-5/src/routes/login.js
@@ -1,3 +1,4 @@
+import React from "react";
 import '../style/login.css'
 
 export const CustomInput = (props) => {
@@ -6,7 +7,7 @@ export const CustomInput = (props) => {
     return(
         <div className='CustomInput'>
             <div>
-                <input type={props.type} placeholder={props.placeholder}/>
+                <input type={props.type} placeholder={props.placeholder} onChange={props.onChange}/>
                 <img src='/assets/icons/delete.svg' />
             </div>
             <hr/>
@@ -22,12 +23,47 @@ export const CustomButton = (props) => {
     const classBtn = `CustomButton ${props.className}`
 
     return(
-        <button className={classBtn}>{props.icon && <img src={iconPath}/>}{props.label}</button>
+        <button className={classBtn}>{props.icon && <img src={iconPath} onChange={props.onChange}/>}{props.label}</button>
     )
 }
 
+
+
 const Login = () => {
 
+    const [email, setEmail] = React.useState("")
+    const [password, setPassword] = React.useState("")
+
+    const [emailError, setEmailError] = React.useState("")
+    const [passwordError, setPasswordError] = React.useState("")
+
+    const onChangeEmail = (event) => {
+        const value = event.target.value;
+
+        if (value.includes("@")===true){
+            setEmail(value)
+            setEmailError("")
+        } else{
+            setEmail(value)
+            setEmailError("이메일 주소를 다시 확인해주세요.")
+        }
+    }
+
+    const onChangePassword = (event) => {
+        if(event.target.value.length >= 8 && event.target.value.length <= 16){
+            setPassword(event.target.value)
+            setPasswordError("")
+        } else {
+            setPasswordError("8-16자의 영문,숫자,특수 문자만 사용 가능합니다.")
+        }
+
+    }
+
+    const onClickLogin = () => {
+        if (email && password){
+            alert("로그인이 완료되었습니다.")
+        }
+    }
 
     return(
         <div className='LoginBackground'>
@@ -40,11 +76,11 @@ const Login = () => {
             </div>
             <div className="LoginField">
                 <div className='LoginInputgroup'>
-                    <CustomInput type="text" placeholder="이메일을 입력해주세요." error="이메일 주소를 다시 확인해주세요." />
-                    <CustomInput type="password" placeholder="비밀번호를 입력해주세요." error="8~16자의 영문, 숫자, 특수 문자만 사용 가능합니다." />
+                    <CustomInput type="text" placeholder="이메일을 입력해주세요." error={emailError} onChange={onChangeEmail}/>
+                    <CustomInput type="password" placeholder="비밀번호를 입력해주세요." error={passwordError} onChange={onChangePassword} />
                 </div>
                 <div className="LoginButtongroup">
-                    <CustomButton className="BtnLogin" label="로그인"/>
+                    <CustomButton className="BtnLogin" label="로그인" onClick={onClickLogin}/>
                     <div className="BtnSecondarygroup">
                         <div>이메일 찾기</div>
                         <div className="verticalLine"></div>
