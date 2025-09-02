4f615ba951e4b82411e42f7d328be8c349ec7b4c
szeunkk <szeunkk@gmail.com>
2025-09-02 18:04:53 +0900

    [트립트립] 게시판 등록 API와 컴포넌트 명명 규칙 동기화

--- maincamp/src/components/ui/input/InputBoardAddress.tsx
+++ maincamp/src/components/ui/input/InputBoardAddress.tsx
@@ -1,21 +1,21 @@
 import cn from "classnames"
-import Zipstyles from './InputZipcode.module.css'
+import Zipstyles from './InputBoardAddress.module.css'
 import Formstyles from './Inputfield.module.css'
 import { ZipcodeProps } from '@/types';
 import Button from '../button/Button'
 
-export default function InputZipcode(props: ZipcodeProps){ 
+export default function InputBoardAddress(props: ZipcodeProps){ 
     return(
         <div className={cn(Formstyles.Formfield, Zipstyles.postForm__address__group)}>
         <div>
             <div><label>주소</label>{props.required && <span>*</span>}</div>
             <div className={Zipstyles.zipCode__group}>
-                <input type="text" placeholder="01234" />
+                <input type="text" id="zipcode" placeholder="01234" onChange={props.onChange}/>
                 <Button variant='FormBtn' type='button'>우편번호 검색</Button>
             </div>
         </div>
-        <input type="text" placeholder={props.placeholder} />
-        {props.placeholder_2 && <input type="text" placeholder={props.placeholder_2}/>}
+        <input type="text" id="address" placeholder={props.placeholder} onChange={props.onChange}/>
+        {props.placeholder_2 && <input type="text" id="addressDetail" placeholder={props.placeholder_2} onChange={props.onChange}/>}
       </div>
     );
 }
