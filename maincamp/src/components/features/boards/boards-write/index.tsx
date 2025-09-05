07dfe5db265010a8e91c51ff2e23fd64a4b5b1a0
szeunkk <szeunkk@gmail.com>
2025-09-05 15:59:31 +0900

    [트립트립] refactor: 게시글 등록, 수정 폼 컴포넌트 추가(BoardWrite)

---
+++ maincamp/src/components/features/boards/boards-write/index.tsx
@@ -0,0 +1,202 @@
+"use client"
+
+import Button from '@/components/ui/button/Button'
+import { Inputfield, Textareafield } from '@/components/ui/input/Inputfield'
+import InputBoardAddress from '@/components/ui/input/InputBoardAddress'
+import InputImage from '@/components/ui/input/InputImage'
+import styles from './index.module.css'
+import Link from 'next/link'
+import { useState } from 'react'
+import { ChangeEvent } from 'react'
+import { gql, useMutation } from '@apollo/client'
+import { useRouter } from 'next/navigation'
+import { CREATE_BOARD } from '@/graphql/mutations/board'
+import { UPLOAD_FILE } from '@/graphql/queries/file'
+
+type ImageUrlArray = (string | null | undefined)[]
+
+export default function BoardsWrite({isEdit}:{isEdit: boolean}){
+
+    /* 게시물 등록 유효성 검사 */
+    // 1. 작성자, 비밀번호, 제목, 컨텐츠 작성 시 setState로 상태 변경
+    const [writer, setWriter] = useState("")
+    const [password, setPassword] = useState("")
+    const [title, setTitle] = useState("")
+    const [contents, setContents] = useState("")
+    const [youtubeUrl, setYoutubeUrl] = useState("")
+    const [zipcode, setZipcode] = useState("")
+    const [address, setAddress] = useState("")
+    const [addressDetail, setAddressDetail] = useState("")
+    const [images, setImages] = useState<ImageUrlArray>([undefined,undefined,undefined])
+
+    // 1-2. 페이지 이동을 위한 useRouter
+    const router = useRouter();
+
+    // 1-3. 게시글 생성 API 요청 함수
+    const [createBoard] = useMutation(CREATE_BOARD)
+
+    // 1-4. 이미지 업로드 API 요청 함수
+    const [uploadFile] = useMutation(UPLOAD_FILE);
+
+
+    // 2. 필수 작성 요소 작성 여부에 따른 버튼 활성화
+    const [isValid, setIsValid] = useState(true)
+
+    // 3. Change Event에 따른 유효성 검증
+    const onChangeWriter = (event: ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value
+        
+        setWriter(value)
+    
+        if(value && password && title && contents){
+          setIsValid(false)
+        } else{
+          setIsValid(true)
+        }
+      }
+      const onChangePassword = (event: ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value
+
+        setPassword(value)
+    
+        if(writer && value && title && contents){
+          setIsValid(false)
+        } else{
+          setIsValid(true)
+        }
+      }
+      const onChangeTitle = (event: ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value
+        setTitle(value)
+    
+        if(writer && password && value && contents){
+          setIsValid(false)
+        } else{
+          setIsValid(true)
+        }
+      }
+      const onChangeContents = (event: ChangeEvent<HTMLTextAreaElement>) => {
+        const value = event.target.value
+        setContents(value)
+    
+        if(writer && password && title && value){
+          setIsValid(false)
+        } else{
+          setIsValid(true)
+        }
+      }
+
+      // 3-1. 필수 요소 아닌 ChangeEvent 추가
+      const onChangeYoutubeUrl = (event: ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value
+        setYoutubeUrl(value)
+      }
+
+      const onChangeBoardAddress = (event: ChangeEvent<HTMLInputElement>) => {
+        const {id, value} = event.target;
+        switch(id){
+          case "zipcode": {setZipcode(value);break;}
+          case "address": {setAddress(value);break;}
+          case "addressDetail": {setAddressDetail(value);break;}
+        }
+      }
+
+      const onChangeFile = async(event: ChangeEvent<HTMLInputElement>) => {
+        const {id, files} = event.target;
+        const file = files?.[0];
+
+        const handleSetImageUrl = (index: number, url: string) => {
+          setImages(
+            prevUrls => {
+              const NewUrls = [...prevUrls]
+              NewUrls[index] = url
+              return NewUrls
+            }
+          )
+        }
+        
+        const result = await uploadFile({
+          variables:{
+            file
+          }
+        });
+
+        const fileUrl = result.data?.uploadFile.url
+
+        switch(id){
+          case "0":{handleSetImageUrl(Number(id), fileUrl)}
+          case "1":{handleSetImageUrl(Number(id), fileUrl)}
+          case "2":{handleSetImageUrl(Number(id), fileUrl)}
+        }
+        
+      }
+
+      // 4. 버튼 활성화 후 등록 버튼 클릭 시 알럿 발생
+      const onClickSubmit = async () => {
+        try{
+          const result = await createBoard({
+            variables:{
+              createBoardInput:{
+                writer: writer,
+                password: password,
+                title: title,
+                contents: contents,
+                youtubeUrl: youtubeUrl,
+                boardAddress: {
+                  zipcode: zipcode,
+                  address: address,
+                  addressDetail: addressDetail,
+                },
+                images: images.filter(Boolean),
+              }
+            }
+          })
+          console.log("🚀 ~ onClickBtn ~ result:", result)
+          const boardId = result.data.createBoard._id
+          router.push(
+            `/boards/${boardId}`
+          )
+
+        } catch (error) {
+          alert("에러가 발생하였습니다. 다시 시도해 주세요.");
+        } 
+      }
+
+      // 5. 취소 버튼 클릭 시 뒤로 가기
+      const onClickCancel = () => {
+        router.back()
+      }
+
+    return(
+        <div className={styles.Formfield}>
+            {/* 폼 타이틀 */}
+            <div className={styles.postForm__title}>게시물 등록</div>
+            {/* 작성자 그룹 */}
+            <div className={styles.postForm__writer__group}>
+                <Inputfield type='text' label='작성자' required placeholder='작성자 명을 입력해 주세요.' isEdit={isEdit} onChange={onChangeWriter}></Inputfield>
+                <Inputfield type='password' label='비밀번호' required placeholder='비밀번호를 입력해 주세요.' isEdit={isEdit} onChange={onChangePassword}></Inputfield>
+            </div>
+            <hr />
+            <Inputfield type='text' label='제목'required placeholder='제목을 입력해 주세요.' onChange={onChangeTitle} ></Inputfield>
+            <hr/>
+            <Textareafield label='내용' required placeholder='내용을 입력해 주세요.' onChange={onChangeContents} ></Textareafield>
+            <hr />
+            <InputBoardAddress placeholder='주소를 입력해 주세요.' placeholder_2='상세주소' onChange={onChangeBoardAddress}></InputBoardAddress>
+            <hr />
+            <Inputfield type='string' label='유튜브 링크' placeholder='링크를 입력해 주세요.' onChange={onChangeYoutubeUrl}></Inputfield>
+            <hr />
+            <div className={styles.postForm__attachments__group}>
+                <label>사진 첨부</label>
+                <div className={styles.image__upload__group}>
+                    {images[0] ? <img src={`https://storage.googleapis.com/${images[0]}`} className={styles.upload__image}/>:<InputImage id="0" onChange={onChangeFile} />}
+                    {images[1] ? <img src={`https://storage.googleapis.com/${images[1]}`} className={styles.upload__image}/>:<InputImage id="1" onChange={onChangeFile} />}
+                    {images[2] ? <img src={`https://storage.googleapis.com/${images[2]}`} className={styles.upload__image}/>:<InputImage id="2" onChange={onChangeFile} />}
+                </div>
+            </div>
+            <div className={styles.postForm__button__group}>
+                <Button type="button" variant='FormBtn' onClick={onClickCancel}>취소</Button>
+                <Button type="submit" variant='FormBtn' disabled={isEdit? false :isValid} onClick={onClickSubmit}>{isEdit? "수정" : "등록"}하기</Button>
+            </div>
+        </div>
+    );
+}
