a65bf1acbb91d7b74d613286eb21dbca4f311515
szeunkk <szeunkk@gmail.com>
2025-09-08 09:38:08 +0900

    [트립트립] feat: 게시글 화면에서 boardId 불러오기

--- maincamp/src/app/boards/[boardId]/edit/page.tsx
+++ maincamp/src/app/boards/[boardId]/edit/page.tsx
@@ -1,11 +1,20 @@
 "use client"
 
 import BoardsWrite from "@/components/features/boards/boards-write";
+import { FETCH_BOARD } from "@/graphql/queries/board";
+import { useQuery } from "@apollo/client";
+import { useParams } from "next/navigation";
 
-export default function BoardsNewPage (){
-
+export default function BoardsEditPage (){
+    const params = useParams();
+    const boardId = params.boardId
+    const { data } = useQuery(FETCH_BOARD, {
+      variables: {
+          boardId: boardId
+      }
+  })
 
     return(
-      <BoardsWrite isEdit={true}/>
+      <BoardsWrite isEdit={true} data={data}/>
     );
 }
