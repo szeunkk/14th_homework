3699b1f28660b100928bb94b9d303e6f0d3bd89b
szeunkk <szeunkk@gmail.com>
2025-09-08 09:36:02 +0900

    [트립트립] feat: 수정하기 버튼 클릭 시 updateBoard API 요청 및 비밀번호 에러 추가

--- maincamp/src/components/features/boards/boards-write/index.tsx
+++ maincamp/src/components/features/boards/boards-write/index.tsx
@@ -5,35 +5,40 @@ import { Inputfield, Textareafield } from '@/components/ui/input/Inputfield'
 import InputBoardAddress from '@/components/ui/input/InputBoardAddress'
 import InputImage from '@/components/ui/input/InputImage'
 import styles from './index.module.css'
-import Link from 'next/link'
 import { useState } from 'react'
 import { ChangeEvent } from 'react'
 import { gql, useMutation } from '@apollo/client'
-import { useRouter } from 'next/navigation'
-import { CREATE_BOARD } from '@/graphql/mutations/board'
+import { useParams, useRouter } from 'next/navigation'
+import { CREATE_BOARD, UPDATE_BOARD } from '@/graphql/mutations/board'
 import { UPLOAD_FILE } from '@/graphql/queries/file'
+import { FETCH_BOARD, FETCH_BOARDS_AND_COUNT } from '@/graphql/queries/board'
 
 type ImageUrlArray = (string | null | undefined)[]
 
-export default function BoardsWrite({isEdit}:{isEdit: boolean}){
+export default function BoardsWrite({isEdit, data}:{isEdit: boolean, data?:any}){
+    // 0. 세팅
+    const router = useRouter();
+    const params = useParams();
 
     /* 게시물 등록 유효성 검사 */
     // 1. 작성자, 비밀번호, 제목, 컨텐츠 작성 시 setState로 상태 변경
-    const [writer, setWriter] = useState("")
-    const [password, setPassword] = useState("")
-    const [title, setTitle] = useState("")
-    const [contents, setContents] = useState("")
-    const [youtubeUrl, setYoutubeUrl] = useState("")
-    const [zipcode, setZipcode] = useState("")
-    const [address, setAddress] = useState("")
-    const [addressDetail, setAddressDetail] = useState("")
-    const [images, setImages] = useState<ImageUrlArray>([undefined,undefined,undefined])
+    const [writer, setWriter] = useState(!data? "" : data.fetchBoard.writer)
+    const [password, setPassword] = useState(!data? "" : data.fetchBoard.password)
+    const [title, setTitle] = useState(!data? "" : data.fetchBoard.title)
+    const [contents, setContents] = useState(!data? "" : data.fetchBoard.contents)
+    const [youtubeUrl, setYoutubeUrl] = useState(!data? "" : data.fetchBoard.youtubeUrl)
+    const [zipcode, setZipcode] = useState(!data? "" : data.fetchBoard.zipcode)
+    const [address, setAddress] = useState(!data? "" : data.fetchBoard.address)
+    const [addressDetail, setAddressDetail] = useState(!data? "" : data.fetchBoard.addressDetail)
+    const [images, setImages] = useState<ImageUrlArray>(!data? [undefined,undefined,undefined] : data.fetchBoard.images)
 
-    // 1-2. 페이지 이동을 위한 useRouter
-    const router = useRouter();
 
-    // 1-3. 게시글 생성 API 요청 함수
+    
+    // 1-2. 게시글 생성 API 요청 함수
     const [createBoard] = useMutation(CREATE_BOARD)
+
+    // 1-3. 게시글 수정 API 요청 함수
+    const [updateBoard] = useMutation(UPDATE_BOARD)
 
     // 1-4. 이미지 업로드 API 요청 함수
     const [uploadFile] = useMutation(UPLOAD_FILE);
@@ -109,29 +114,32 @@ export default function BoardsWrite({isEdit}:{isEdit: boolean}){
           setImages(
             prevUrls => {
               const NewUrls = [...prevUrls]
+              console.log(NewUrls)
               NewUrls[index] = url
               return NewUrls
             }
           )
+          console.log(images)
         }
         
         const result = await uploadFile({
           variables:{
             file
           }
-        });
+        }
+      );
 
         const fileUrl = result.data?.uploadFile.url
 
         switch(id){
-          case "0":{handleSetImageUrl(Number(id), fileUrl)}
-          case "1":{handleSetImageUrl(Number(id), fileUrl)}
-          case "2":{handleSetImageUrl(Number(id), fileUrl)}
+          case "0":{handleSetImageUrl(Number(id), fileUrl);break;}
+          case "1":{handleSetImageUrl(Number(id), fileUrl);break;}
+          case "2":{handleSetImageUrl(Number(id), fileUrl);break;}
         }
         
       }
 
-      // 4. 버튼 활성화 후 등록 버튼 클릭 시 알럿 발생
+      // 4. 등록하기 버튼
       const onClickSubmit = async () => {
         try{
           const result = await createBoard({
@@ -162,7 +170,52 @@ export default function BoardsWrite({isEdit}:{isEdit: boolean}){
         } 
       }
 
-      // 5. 취소 버튼 클릭 시 뒤로 가기
+      // 5. 수정하기 버튼
+      // 수정 버튼 클릭 시 updateBoard 진행
+      const onClickUpdate = async() => {
+              // 5-1. 수정된 사항만 업데이트 될 수 있도록 variables 설정
+      const updateBoardInput ={}
+      if (title!==data?.fetchBoard.title) updateBoardInput.title = title;
+      if (contents!==data?.fetchBoard.contents) updateBoardInput.contents = contents;
+      if (youtubeUrl!==data?.fetchBoard.youtubeUrl) updateBoardInput.youtubeUrl = youtubeUrl;
+      if (zipcode !== data.fetchBoard.zipcode || address!==data.fetchBoard.address || addressDetail!==data.fetchBoard.addressDetail){
+        updateBoardInput.boardAddress = {};
+        if (zipcode!==data.fetchBoard.zipcode) updateBoardInput.boardAddress.zipcode = zipcode;
+        if (address!==data.fetchBoard.address) updateBoardInput.boardAddress.address = address;
+        if (addressDetail!==data.fetchBoard.addressDetail) updateBoardInput.boardAddress.addressDetail = addressDetail;
+      }
+      if (images.filter(Boolean)) {
+        updateBoardInput.images = images.filter(Boolean);
+        console.log(updateBoardInput)
+      }
+      console.log(updateBoardInput)
+        try{
+          const password = prompt("글을 입력할때 입력하셨던 비밀번호를 입력해주세요")
+          const result = await updateBoard({
+            variables: {
+              updateBoardInput: updateBoardInput,
+              password: password,
+              boardId: params.boardId,
+            },
+            refetchQueries: [
+              {
+                query: FETCH_BOARD,
+                variables: { boardId: params.boardId }
+              },
+            ]
+          })
+          console.log(result)
+          router.push(
+            `/boards/${result?.data.updateBoard._id}`
+          )
+        } catch (error) {
+          alert(error.message)
+
+        }
+        
+      }
+
+      // 6. 취소 버튼 클릭 시 뒤로 가기
       const onClickCancel = () => {
         router.back()
       }
@@ -173,17 +226,17 @@ export default function BoardsWrite({isEdit}:{isEdit: boolean}){
             <div className={styles.postForm__title}>게시물 등록</div>
             {/* 작성자 그룹 */}
             <div className={styles.postForm__writer__group}>
-                <Inputfield type='text' label='작성자' required placeholder='작성자 명을 입력해 주세요.' isEdit={isEdit} onChange={onChangeWriter}></Inputfield>
+                <Inputfield type='text' label='작성자' required placeholder='작성자 명을 입력해 주세요.' defaultValue={data?.fetchBoard.writer} isEdit={isEdit} onChange={onChangeWriter}></Inputfield>
                 <Inputfield type='password' label='비밀번호' required placeholder='비밀번호를 입력해 주세요.' isEdit={isEdit} onChange={onChangePassword}></Inputfield>
             </div>
             <hr />
-            <Inputfield type='text' label='제목'required placeholder='제목을 입력해 주세요.' onChange={onChangeTitle} ></Inputfield>
+            <Inputfield type='text' label='제목'required placeholder='제목을 입력해 주세요.' defaultValue={data?.fetchBoard.title} onChange={onChangeTitle} ></Inputfield>
             <hr/>
-            <Textareafield label='내용' required placeholder='내용을 입력해 주세요.' onChange={onChangeContents} ></Textareafield>
+            <Textareafield label='내용' required placeholder='내용을 입력해 주세요.' defaultValue={data?.fetchBoard.contents} onChange={onChangeContents} ></Textareafield>
             <hr />
-            <InputBoardAddress placeholder='주소를 입력해 주세요.' placeholder_2='상세주소' onChange={onChangeBoardAddress}></InputBoardAddress>
+            <InputBoardAddress placeholder='주소를 입력해 주세요.' placeholder_2='상세주소' defaultValue={data?.fetchBoard} onChange={onChangeBoardAddress}></InputBoardAddress>
             <hr />
-            <Inputfield type='string' label='유튜브 링크' placeholder='링크를 입력해 주세요.' onChange={onChangeYoutubeUrl}></Inputfield>
+            <Inputfield type='string' label='유튜브 링크' placeholder='링크를 입력해 주세요.' defaultValue={data?.fetchBoard.youtubeUrl} onChange={onChangeYoutubeUrl}></Inputfield>
             <hr />
             <div className={styles.postForm__attachments__group}>
                 <label>사진 첨부</label>
@@ -195,7 +248,7 @@ export default function BoardsWrite({isEdit}:{isEdit: boolean}){
             </div>
             <div className={styles.postForm__button__group}>
                 <Button type="button" variant='FormBtn' onClick={onClickCancel}>취소</Button>
-                <Button type="submit" variant='FormBtn' disabled={isEdit? false :isValid} onClick={onClickSubmit}>{isEdit? "수정" : "등록"}하기</Button>
+                <Button type="submit" variant='FormBtn' disabled={isEdit? false :isValid} onClick={isEdit? onClickUpdate :onClickSubmit}>{isEdit? "수정" : "등록"}하기</Button>
             </div>
         </div>
     );
