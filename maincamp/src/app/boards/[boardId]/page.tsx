e2482e7848380ab946d89cddcd35f4e1aa305dba
szeunkk <szeunkk@gmail.com>
2025-09-09 21:09:19 +0900

    [트립트립] refactor: 게시글 조회 page.tsx에 댓글 작성, 댓글 조회 컴포넌트 추가

--- maincamp/src/app/boards/[boardId]/page.tsx
+++ maincamp/src/app/boards/[boardId]/page.tsx
@@ -1,11 +1,19 @@
 "use client"
 
+import CommentWrite from "@/components/features/boards/boards-detail/comment-write";
 import BoardsBoardDetailPage from "@/components/features/boards/boards-detail/detail";
+import styles from './styles.module.css'
+import CommentList from "@/components/features/boards/boards-detail/comment-list";
 
 export default function BoardsBoardIdPage() {
 
 
     return(
-        <BoardsBoardDetailPage />
+        <div className={styles.boardsDetail}>
+            <BoardsBoardDetailPage />
+            <CommentWrite />
+            <CommentList />
+        </div>
+
     )
 }
