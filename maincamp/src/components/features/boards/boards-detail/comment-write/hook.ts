465d716fc3eda36e08d846355108c049ae6a400e
szeunkk <szeunkk@gmail.com>
2025-09-09 21:10:55 +0900

    [트립트립] feat: 댓글 작성 컴포넌트 추가

---
+++ maincamp/src/components/features/boards/boards-detail/comment-write/hook.ts
@@ -0,0 +1,85 @@
+import { CreateBoardCommentDocument, FetchBoardCommentsDocument } from "@/commons/graphql/graphql";
+import { useMutation } from "@apollo/client";
+import { getDefaultFetchPolicy } from "@apollo/client/react/hooks/useQuery";
+import { useParams } from "next/navigation"
+import { ChangeEvent, useState } from "react";
+
+export default function useCommentWrite(){
+    
+    // 세팅 - params
+    const params = useParams();
+
+    // 댓글 등록 유효성 검사
+    const [writer, setWriter] = useState("")
+    const [password, setPassword] = useState("")
+    const [contents, setContents] = useState("")
+    const [isValid, setIsValid] = useState(true)
+
+    // 게시글 댓글 생성 API
+    const [createBoardComment] = useMutation(CreateBoardCommentDocument)
+
+    // ChangeEvent에 따른 유효성 검증
+    const onChangeWriter = (event: ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value
+        setWriter(value)
+
+        if(value && password && contents){
+            setIsValid(false)
+        } else {
+            setIsValid(true)
+        }
+    }
+
+    const onChangePassword = (event: ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value
+        setPassword(value)
+
+        if(writer && value && contents){
+            setIsValid(false)
+        } else {
+            setIsValid(true)
+        }
+    }
+
+    const onChangeContents = (event: ChangeEvent<HTMLTextAreaElement>) => {
+        const value = event.target.value
+        setContents(value)
+
+        if(writer && password && value){
+            setIsValid(false)
+        } else {
+            setIsValid(true)
+        }
+    }
+
+    // 댓글 등록하기 버튼 클릭
+    const onClickCommentSubmit = () => {
+        try {
+            const result = createBoardComment({
+                variables:{
+                    createBoardCommentInput:{
+                        writer,
+                        password,
+                        contents,
+                        rating: 0
+                    },
+                    boardId: params.boardId as string,
+                },
+                refetchQueries: [
+                    {
+                        query: FetchBoardCommentsDocument,
+                        variables: { boardId: params.boardId}
+                    },
+                ]
+            })
+            setWriter("")
+            setPassword("")
+            setContents("")
+            setIsValid(true)
+        } catch (error) {
+            alert("에러가 발생하였습니다. 다시 시도해 주세요.")
+        }
+    }
+
+    return{writer, password, contents, isValid, onChangeWriter, onChangePassword, onChangeContents, onClickCommentSubmit}
+}
