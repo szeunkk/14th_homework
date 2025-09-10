d688f32fcfd4cc5da41d0faa0ead36c87e4ed1bd
szeunkk <szeunkk@gmail.com>
2025-09-11 00:13:27 +0900

    [트립트립] feat: 게시글 목록 page 배너 컴포넌트 조립

--- maincamp/src/app/boards/page.tsx
+++ maincamp/src/app/boards/page.tsx
@@ -1,11 +1,16 @@
 "use client"
 
 import BoardsList from "@/components/features/boards/boards-list/list"
+import BoardsBanner from "@/components/features/boards/boards-list/banner"
+import styles from './styles.module.css'
 
 
 export default function BoardsPage () {
 
     return(
-        <BoardsList />
+        <div className={styles.boards}>
+            <BoardsBanner />
+            <BoardsList />
+        </div>
     )
 }
