0aa92765a109de661c697096d2f343c5d19cfeb2
szeunkk <szeunkk@gmail.com>
2025-09-11 00:12:04 +0900

    [트립트립] feat: 게시글 목록 위 배너 기능 구현(swiper 라이브러리)

---
+++ maincamp/src/components/features/boards/boards-list/banner/index.tsx
@@ -0,0 +1,57 @@
+"use client"
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import Image from 'next/image';
+import { Autoplay, Pagination } from 'swiper/modules';
+import styles from './style.module.css'
+
+
+export default function BoardsBanner(){
+    return(
+        <>
+        <Swiper
+            pagination={{
+                clickable: true,
+                bulletActiveClass: styles.swiperPaginationBulletActive,
+            }} 
+            modules={[Autoplay, Pagination]}
+            autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+            }}
+            style={{width:"100%"}}>
+            <SwiperSlide>
+                <Image
+                    src='/images/bannerimage1.svg'
+                    alt='배너이미지1'
+                    width={0}
+                    height={0}
+                    sizes='100vw'
+                    style={{width: "100vw", height:"32rem", objectFit:"cover", objectPosition:"50% 90%"}}    
+                />
+            </SwiperSlide>
+            <SwiperSlide>
+                <Image
+                    src='/images/bannerimage2.svg'
+                    alt='배너이미지2'
+                    width={0}
+                    height={0}
+                    sizes='100vw'
+                    style={{width: "100vw", height:"32rem", objectFit:"cover", objectPosition:"50% 80%"}}    
+                />
+            </SwiperSlide>
+            <SwiperSlide>
+                <Image
+                    src='/images/bannerimage3.svg'
+                    alt='배너이미지3'
+                    width={0}
+                    height={0}
+                    sizes='100vw'
+                    style={{width: "100vw", height:"32rem", objectFit:"cover", objectPosition:"50% 55%"}}    
+                />
+            </SwiperSlide>
+        </Swiper>
+        </>
+    )
+}
