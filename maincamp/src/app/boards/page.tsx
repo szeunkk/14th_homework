1734ece20f0c4d6b7e8105162b9284edf55d8d93
szeunkk <szeunkk@gmail.com>
2025-09-10 21:42:57 +0900

    [트립트립] refactor: 게시글 목록 컴포넌트 분리(배너, 목록)을 위한 폴더 구조 변경

--- maincamp/src/app/boards/page.tsx
+++ maincamp/src/app/boards/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import BoardsList from "@/components/features/boards/boards-list"
+import BoardsList from "@/components/features/boards/boards-list/list"
 
 
 export default function BoardsPage () {
