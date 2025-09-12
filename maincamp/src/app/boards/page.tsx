5a386fc3cd31e7d40d3fb939abe60d48fdf55304
szeunkk <szeunkk@gmail.com>
2025-09-12 17:33:06 +0900

    [트립트립] refactor: 커스텀 레이아웃 그룹화에 따른 기존 boardlist page 수정

--- maincamp/src/app/boards/page.tsx
+++ maincamp/src/app/boards/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import BoardsList from "@/components/features/boards/boards-list/list"
-import BoardsBanner from "@/components/features/boards/boards-list/banner"
+import BoardsBanner from "@/commons/layout/banner"
 import styles from './styles.module.css'
 
 
@@ -9,7 +9,6 @@ export default function BoardsPage () {
 
     return(
         <div className={styles.boards}>
-            <BoardsBanner />
             <BoardsList />
         </div>
     )
