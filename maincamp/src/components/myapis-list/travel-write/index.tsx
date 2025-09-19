18876ea51cdfdc5c784377292d2532b2eface0fc
szeunkk <szeunkk@gmail.com>
2025-09-19 17:53:36 +0900

    [트립트립] feat: 여행지 추가 및 supabase 연결

---
+++ maincamp/src/components/myapis-list/travel-write/index.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import Button from "@/components/ui/button/Button";
+import { Inputfield } from "@/components/ui/input/Inputfield";
+import styles from "./style.module.css";
+import useTravelWrite from "./hook";
+
+export default function TravelWrite() {
+  const { inputs, onChangeInputs, onClickSubmit, isValid } = useTravelWrite();
+
+  return (
+    <div className={styles.TravelWriteForm}>
+      <Inputfield
+        type="text"
+        placeholder="여행지를 선택해주세요"
+        label="여행국가"
+        id="destination"
+        onChange={onChangeInputs}
+        value={inputs?.destination}
+      />
+      <Inputfield
+        type="text"
+        placeholder="이번 여행에 대한 간단한 제목을 입력해주세요"
+        label="여행 제목"
+        id="title"
+        onChange={onChangeInputs}
+        value={inputs?.title}
+      />
+      <div className={styles.DateGroup}>
+        <Inputfield
+          type="date"
+          placeholder="YYYY.MM.DD"
+          label="시작일"
+          id="startDate"
+          onChange={onChangeInputs}
+          value={inputs?.startDate}
+        />
+        <Inputfield
+          type="date"
+          placeholder="YYYY.MM.DD"
+          label="종료일"
+          id="endDate"
+          onChange={onChangeInputs}
+          value={inputs?.endDate}
+        />
+      </div>
+      <div className={styles.ButtonGroup}>
+        <Button variant="FormBtn" type="button">
+          취소
+        </Button>
+        <Button
+          variant="FormBtn"
+          type="submit"
+          disabled={isValid}
+          onClick={onClickSubmit}
+        >
+          등록하기
+        </Button>
+      </div>
+    </div>
+  );
+}
