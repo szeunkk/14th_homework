b2e5df397274134e93a4ef429067579fcbb8601d
szeunkk <szeunkk@gmail.com>
2025-09-21 23:22:07 +0900

    [트립트립] feat: myapis 여행 기록 페이지 및 상세페이지 컴포넌트 조립

--- maincamp/src/app/myapis/page.tsx
+++ maincamp/src/app/myapis/page.tsx
@@ -1,14 +1,19 @@
+"use client";
+
 import TravelList from "@/components/myapis-list/travel-list";
 import TravelWrite from "@/components/myapis-list/travel-write";
 import Button from "@/components/ui/button/Button";
+import { useState } from "react";
 
 export default function MyapisPage() {
+  const [visible, setVisible] = useState(false);
+
   return (
     <>
-      <Button variant="FormBtn" type="button">
+      <Button variant="FormBtn" type="button" onClick={() => setVisible(true)}>
         여행등록하기
       </Button>
-      <TravelWrite></TravelWrite>
+      {visible && <TravelWrite setVisible={setVisible}></TravelWrite>}
       <TravelList></TravelList>
     </>
   );
