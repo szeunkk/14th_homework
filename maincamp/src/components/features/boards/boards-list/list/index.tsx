927e56857759fbd15cb027984aa82c8c72231bf4
szeunkk <szeunkk@gmail.com>
2025-09-23 16:38:42 +0900

    [트립트립] feat: 검색 결과 유무에 따른 안내 문구 표시 및 CSS 수정

--- maincamp/src/components/features/boards/boards-list/list/index.tsx
+++ maincamp/src/components/features/boards/boards-list/list/index.tsx
@@ -1,33 +1,30 @@
-"use client"
+"use client";
 
-import styles from './styles.module.css'
-import ListRow from "@/components/ui/list/ListRow"
-import { Fragment } from "react"
-import { BoardType, IBoardsListProps } from './types'
+import styles from "./styles.module.css";
+import ListRow from "@/components/ui/list/ListRow";
+import { Fragment } from "react";
+import { BoardType, IBoardsListProps } from "./types";
 
-
-export default function BoardsList (props: IBoardsListProps) {
-
-    return(
-            <div className={styles.ListRows}>
-                {props.data?.map((el: BoardType, index: number) => {
-                    return(
-                        <Fragment key={el._id}>
-                        <ListRow
-                            num={props.pageNum[index]}
-                            flex={props.flex}
-                            textAlign={props.textAlign}
-                            _id={el._id}
-                            title={el.title}
-                            writer={el.writer as string}
-                            createdAt={el.createdAt}
-                            currentPage={props.currentPage}
-                            refetch={props.refetch}
-                        >
-                        </ListRow>
-                        </Fragment>
-                    )
-                })}
-            </div>
-    )
+export default function BoardsList(props: IBoardsListProps) {
+  return (
+    <div className={styles.ListRows}>
+      {props.data && props.data.length > 0 ? (
+        props.data?.map((el: BoardType, index: number) => (
+          <ListRow
+            num={props.pageNum[index]}
+            flex={props.flex}
+            textAlign={props.textAlign}
+            _id={el._id}
+            title={el.title}
+            writer={el.writer as string}
+            createdAt={el.createdAt}
+            currentPage={props.currentPage}
+            refetch={props.refetch}
+          ></ListRow>
+        ))
+      ) : (
+        <div className={styles.noSearchResult}>검색 결과가 없습니다.</div>
+      )}
+    </div>
+  );
 }
