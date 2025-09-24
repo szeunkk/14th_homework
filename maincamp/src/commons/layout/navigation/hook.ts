bc7ef0c7d2987e94030da2923f9782a3789aef9e
szeunkk <szeunkk@gmail.com>
2025-09-25 03:30:11 +0900

    [트립트립] feat: 로그인 상태에 따른 네비게이션 아이콘 조건부 렌더링 구현 및 pathname에 따른 네비게이션 css 구현

---
+++ maincamp/src/commons/layout/navigation/hook.ts
@@ -0,0 +1,19 @@
+import { FETCH_USER_LOGGED_IN } from "@/graphql/queries/login";
+import { useQuery } from "@apollo/client";
+import { usePathname, useRouter } from "next/navigation";
+
+export default function useNavigation() {
+  const router = useRouter();
+
+  const pathname = usePathname();
+
+  const { data } = useQuery(FETCH_USER_LOGGED_IN);
+  const onClickBoards = () => {
+    router.push("/boards");
+  };
+  const onClickLogin = () => {
+    router.push("/login");
+  };
+
+  return { onClickBoards, onClickLogin, data, pathname };
+}
