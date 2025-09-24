2144540d8f37dd6f2b4dca80e8b0a1ae85f4f5f6
szeunkk <szeunkk@gmail.com>
2025-09-24 23:04:39 +0900

    [트립트립] feat: 로그인 기능 구현 및 이메일, 비밀번호 미입력 시 error 문구 구현

--- maincamp/src/components/users/login/hook.ts
+++ maincamp/src/components/users/login/hook.ts
@@ -1,13 +1,83 @@
+import { LoginUserDocument } from "@/commons/graphql/graphql";
+import { useAccessTokenStore } from "@/commons/stores/accessTokenStore";
+import { useMutation, useQuery } from "@apollo/client";
+import { Modal } from "antd";
 import { useRouter } from "next/navigation";
+import { ChangeEvent, useState } from "react";
 
 export default function useLogin() {
+  // 페이지 이동을 위한 라우터
   const router = useRouter();
 
-  const;
+  // state 설정: Email, password, isValid
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isValid, setIsValid] = useState(true);
+
+  // 글로벌 state: setAccessToken
+  const { setAccessToken } = useAccessTokenStore();
+
+  // 로그인 mutation
+  const [loginUser] = useMutation(LoginUserDocument);
+
+  // onChange 함수
+  const onChangeEmail = (event: ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+
+    if (event.target.value && password) {
+      setIsValid(true);
+    }
+  };
+
+  const onChangePassword = (event: ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+
+    if (event.target.value && password) {
+      setIsValid(true);
+    }
+  };
+
+  // onClick 함수
+  const onCLickLogin = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (email.trim() === "" || password.trim() === "") {
+      setIsValid(false);
+      return;
+    }
+
+    try {
+      const result = await loginUser({
+        variables: { email, password },
+      });
+
+      const accessToken = result.data?.loginUser.accessToken ?? "";
+      setAccessToken(accessToken);
+
+      setEmail("");
+      setPassword("");
+
+      router.push("/boards");
+    } catch (error) {
+      const showErrorModal = () => {
+        Modal.error({
+          title: "로그인에 실패하였습니다.",
+          content: error as string,
+        });
+        showErrorModal();
+      };
+    }
+  };
 
   const onClickSignup = () => {
     router.push("/signup");
   };
 
-  return { onClickSignup };
+  return {
+    onClickSignup,
+    onChangeEmail,
+    onChangePassword,
+    onCLickLogin,
+    isValid,
+  };
 }
--- maincamp/src/components/users/login/index.tsx
+++ maincamp/src/components/users/login/index.tsx
@@ -6,19 +6,41 @@ import styles from "./styles.module.css";
 import useLogin from "./hook";
 
 export default function Login() {
-  const { onClickSignup } = useLogin();
+  const {
+    onClickSignup,
+    onChangeEmail,
+    onChangePassword,
+    onCLickLogin,
+    isValid,
+  } = useLogin();
   return (
-    <form className={styles.formContainer}>
+    <form className={styles.formContainer} onSubmit={onCLickLogin}>
       <>
         <img src="/icons/triptrip_logo.svg" />
         <span>트립트립에 오신걸 환영합니다.</span>
       </>
       <div>
         <p>트립트립에 로그인 하세요.</p>
-        <div className={styles.inputContainer}>
-          <Inputfield type="text" placeholder="이메일을 입력해 주세요." />
-          <Inputfield type="password" placeholder="비밀번호를 입력해 주세요." />
-          <span>아이디 또는 비밀번호를 확인해 주세요.</span>
+        <div
+          className={
+            isValid
+              ? `${styles.inputContainer}`
+              : `${styles.inputContainer}  ${styles.error}`
+          }
+        >
+          <Inputfield
+            type="text"
+            placeholder="이메일을 입력해 주세요."
+            onChange={onChangeEmail}
+          />
+          <Inputfield
+            type="password"
+            placeholder="비밀번호를 입력해 주세요."
+            onChange={onChangePassword}
+          />
+          <span style={{ display: isValid ? "none" : "block" }}>
+            아이디 또는 비밀번호를 확인해 주세요.
+          </span>
         </div>
       </div>
       <>
