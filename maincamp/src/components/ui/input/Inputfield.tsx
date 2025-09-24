057baaaf096078a41b285e7bbf1bc283a4b1a065
szeunkk <szeunkk@gmail.com>
2025-09-24 21:23:01 +0900

    [트립트립] fix: prettier 및 figma와 동일하게 컴포넌트 값 수정

--- maincamp/src/components/ui/input/Inputfield.tsx
+++ maincamp/src/components/ui/input/Inputfield.tsx
@@ -1,26 +1,49 @@
-import styles from './Inputfield.module.css'
+import styles from "./Inputfield.module.css";
 import { InputProps, TextareaProps } from "@/types/index";
 
-
 /* input 컴포넌트: 라벨, span, input */
-export function Inputfield (props: InputProps) {
-
-    return(
-        <div className={styles.Formfield}>
-            <div><label>{props.label}</label>{props.required && <span>*</span>}</div>
-            <input type={props.type} placeholder={props.placeholder} id={props.id} value={props.type === "password" && props.isEdit ? "*********" : props.value} disabled={props.isEdit} onChange={props.onChange} />
-        </div>
-    );
+export function Inputfield(props: InputProps) {
+  return (
+    <div className={styles.Formfield}>
+      <div>
+        <label>{props.label}</label>
+        {props.required && <span>*</span>}
+      </div>
+      <input
+        type={props.type}
+        placeholder={props.placeholder}
+        id={props.id}
+        value={
+          props.type === "password" && props.isEdit ? "*********" : props.value
+        }
+        disabled={props.isEdit}
+        onChange={props.onChange}
+      />
+    </div>
+  );
 }
 
-
 /* TextArea 컴포넌트: 라벨, span, input */
-export function Textareafield (props: TextareaProps){
-
-    return(
-        <div className={props.isCommentField? styles.Commentfield :styles.Formfield}>
-            {props.isCommentField? "" : <div><label>{props.label}</label>{props.required && <span>*</span>}</div>}
-            <textarea placeholder={props.placeholder} id={props.id} value={props.value} onChange={props.onChange} maxLength={props.maxLength}></textarea>
+export function Textareafield(props: TextareaProps) {
+  return (
+    <div
+      className={props.isCommentField ? styles.Commentfield : styles.Formfield}
+    >
+      {props.isCommentField ? (
+        ""
+      ) : (
+        <div>
+          <label>{props.label}</label>
+          {props.required && <span>*</span>}
         </div>
-    );
+      )}
+      <textarea
+        placeholder={props.placeholder}
+        id={props.id}
+        value={props.value}
+        onChange={props.onChange}
+        maxLength={props.maxLength}
+      ></textarea>
+    </div>
+  );
 }
