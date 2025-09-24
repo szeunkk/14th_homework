fce18c5a625b0dc4f187d18e1567efc6d2a0e5da
szeunkk <szeunkk@gmail.com>
2025-09-24 21:23:21 +0900

    [트립트립] feat: 로그인, 회원가입 페이지

---
+++ maincamp/src/app/login/page.tsx
@@ -0,0 +1,5 @@
+import Login from "@/components/users/login";
+
+export default function LoginPage() {
+  return <Login />;
+}
---
+++ maincamp/src/app/signup/page.tsx
@@ -0,0 +1,5 @@
+import Signup from "@/components/users/signup";
+
+export default function SignUpPage() {
+  return <Signup />;
+}
