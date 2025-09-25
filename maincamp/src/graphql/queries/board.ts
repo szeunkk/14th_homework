019cd7cb97e6f4d79775ea3b2f1ff1dae47aa73e
szeunkk <szeunkk@gmail.com>
2025-09-25 18:14:12 +0900

    [트립트립] fix: 쿼리 수정

--- maincamp/src/graphql/queries/board.ts
+++ maincamp/src/graphql/queries/board.ts
@@ -1,63 +1,91 @@
 import { gql } from "@apollo/client";
 
 export const FETCH_BOARD = gql`
-    query fetchBoard($boardId: ID!) {
-        fetchBoard(boardId: $boardId){
-            writer
-            title
-            contents
-            youtubeUrl
-            images
-            createdAt
-            likeCount
-            dislikeCount
-            boardAddress{zipcode, address, addressDetail}
-        }
+  query fetchBoard($boardId: ID!) {
+    fetchBoard(boardId: $boardId) {
+      writer
+      title
+      contents
+      youtubeUrl
+      images
+      createdAt
+      likeCount
+      dislikeCount
+      boardAddress {
+        zipcode
+        address
+        addressDetail
+      }
+      user
     }
-`
-
+  }
+`;
 
 export const FETCH_BOARDS = gql`
-    query fetchBoards($endDate: DateTime, $startDate: DateTime, $search: String, $page: Int){
-        fetchBoards(endDate: $endDate, startDate: $startDate, search: $search, page: $page){
-            _id
-            writer
-            title
-            createdAt
-            deletedAt
-        }
+  query fetchBoards(
+    $endDate: DateTime
+    $startDate: DateTime
+    $search: String
+    $page: Int
+  ) {
+    fetchBoards(
+      endDate: $endDate
+      startDate: $startDate
+      search: $search
+      page: $page
+    ) {
+      _id
+      writer
+      title
+      createdAt
+      deletedAt
+      user
     }
-`
+  }
+`;
 
 export const FETCH_BOARDS_COUNT = gql`
-    query fetchBoardsCount($endDate: DateTime, $startDate: DateTime, $search: String){
-        fetchBoardsCount(endDate: $endDate, startDate: $startDate, search: $search)
-    }
-`
+  query fetchBoardsCount(
+    $endDate: DateTime
+    $startDate: DateTime
+    $search: String
+  ) {
+    fetchBoardsCount(endDate: $endDate, startDate: $startDate, search: $search)
+  }
+`;
 
 export const FETCH_BOARDS_AND_COUNT = gql`
-    query 
-        fetchBoardsAndCount($endDate: DateTime, $startDate: DateTime, $search: String, $page: Int)
-        {
-        fetchBoards(endDate: $endDate, startDate: $startDate, search: $search, page: $page) {
-            _id
-            writer
-            title
-            createdAt
-            deletedAt
-        }
-        fetchBoardsCount(endDate: $endDate, startDate: $startDate, search: $search)
-}
-`
+  query fetchBoardsAndCount(
+    $endDate: DateTime
+    $startDate: DateTime
+    $search: String
+    $page: Int
+  ) {
+    fetchBoards(
+      endDate: $endDate
+      startDate: $startDate
+      search: $search
+      page: $page
+    ) {
+      _id
+      writer
+      title
+      createdAt
+      deletedAt
+    }
+    fetchBoardsCount(endDate: $endDate, startDate: $startDate, search: $search)
+  }
+`;
 
 export const FETCH_BOARD_COMMENTS = gql`
-    query fetchBoardComments($page: Int, $boardId: ID!) {
-        fetchBoardComments(page: $page, boardId: $boardId) {
-            _id
-            writer
-            contents
-            rating
-            createdAt
-        }
+  query fetchBoardComments($page: Int, $boardId: ID!) {
+    fetchBoardComments(page: $page, boardId: $boardId) {
+      _id
+      writer
+      contents
+      rating
+      createdAt
+      user
     }
-`
+  }
+`;
