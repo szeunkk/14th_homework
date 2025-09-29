b3e6280cce0e15d1e0aee96006f74f97078a9137
szeunkk <szeunkk@gmail.com>
2025-09-29 22:31:01 +0900

    [트립트립] feat: input 및 textarea에서 isAuth 값에 따른 기능 추가

--- maincamp/src/components/ui/input/Inputfield.tsx
+++ maincamp/src/components/ui/input/Inputfield.tsx
@@ -1,8 +1,20 @@
 import styles from "./Inputfield.module.css";
 import { InputProps, TextareaProps } from "@/types/index";
+import { Modal } from "antd";
+import { useRouter } from "next/navigation";
 
 /* input 컴포넌트: 라벨, span, input */
 export function Inputfield(props: InputProps) {
+  const router = useRouter();
+  const handleUnauthClick = () => {
+    Modal.error({
+      title: "로그인 후 이용할 수 있습니다.",
+      content: "댓글 기능은 로그인 후 이용할 수 있습니다.",
+    });
+
+    router.push("/login");
+  };
+
   return (
     <div className={styles.Formfield}>
       <div>
@@ -16,8 +28,10 @@ export function Inputfield(props: InputProps) {
         value={
           props.type === "password" && props.isEdit ? "*********" : props.value
         }
+        readOnly={props.isAuth === false ? true : false}
         disabled={props.isEdit}
         onChange={props.onChange}
+        onClick={props.isAuth === false ? handleUnauthClick : undefined}
       />
     </div>
   );
@@ -25,6 +39,17 @@ export function Inputfield(props: InputProps) {
 
 /* TextArea 컴포넌트: 라벨, span, input */
 export function Textareafield(props: TextareaProps) {
+  const router = useRouter();
+
+  const handleUnauthClick = () => {
+    Modal.error({
+      title: "로그인 후 이용할 수 있습니다.",
+      content: "댓글 기능은 로그인 후 이용할 수 있습니다.",
+    });
+
+    router.push("/login");
+  };
+
   return (
     <div
       className={props.isCommentField ? styles.Commentfield : styles.Formfield}
@@ -42,7 +67,9 @@ export function Textareafield(props: TextareaProps) {
         id={props.id}
         value={props.value}
         onChange={props.onChange}
+        readOnly={props.isAuth === false ? true : false}
         maxLength={props.maxLength}
+        onClick={props.isAuth === false ? handleUnauthClick : undefined}
       ></textarea>
     </div>
   );
