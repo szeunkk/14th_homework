f9beacdaf2d41722e752b469a685256cb61871a0
szeunkk <szeunkk@gmail.com>
2025-09-29 21:32:43 +0900

    [트립트립] feat: 기존 mypage 로그인 인가 확인 -> HOC 활용하여 변경

--- maincamp/src/app/mypage/page.tsx
+++ maincamp/src/app/mypage/page.tsx
@@ -3,9 +3,26 @@
 import { withAuth } from "@/commons/hocs/withAuth";
 import { FETCH_USER_LOGGED_IN } from "@/graphql/queries/login";
 import { useQuery } from "@apollo/client";
+import { Modal } from "antd";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
-export default withAuth(function MyPagePage() {
+export default withAuth(function MyPagePage({ isAuth }: { isAuth?: boolean }) {
+  const router = useRouter();
   const { data } = useQuery(FETCH_USER_LOGGED_IN);
+
+  useEffect(() => {
+    if (isAuth === false) {
+      console.log(isAuth);
+      Modal.error({
+        title: "해당 페이지에 접속 권한이 없습니다.",
+        content: "로그인 후 이용해주세요.",
+      });
+      router.push("/login");
+    }
+  }, [isAuth, router]);
+
+  if (isAuth === false) return null;
 
   return <h1>마이페이지 입니다... 구현 예정 ... </h1>;
 });
