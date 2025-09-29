54f3461d62ada6fa111b0eea99d5bdf24b3e3086
szeunkk <szeunkk@gmail.com>
2025-09-29 17:19:15 +0900

    [트립트립] fix: 게시글 작성 주소 css 일부 수정

--- maincamp/src/components/ui/input/InputBoardAddress.tsx
+++ maincamp/src/components/ui/input/InputBoardAddress.tsx
@@ -1,23 +1,53 @@
-import cn from "classnames"
-import Zipstyles from './InputBoardAddress.module.css'
-import Formstyles from './Inputfield.module.css'
-import { ZipcodeProps } from '@/types';
-import Button from '../button/Button'
+import cn from "classnames";
+import Zipstyles from "./InputBoardAddress.module.css";
+import Formstyles from "./Inputfield.module.css";
+import { ZipcodeProps } from "@/types";
+import Button from "../button/Button";
+import styles from "./InputBoardAddress.module.css";
 
-export default function InputBoardAddress(props: ZipcodeProps){ 
-    const {zipcode, address, addressDetail} = props.value
+export default function InputBoardAddress(props: ZipcodeProps) {
+  const { zipcode, address, addressDetail } = props.value;
 
-    return(
-        <div className={cn(Formstyles.Formfield, Zipstyles.postForm__address__group)}>
+  return (
+    <div
+      className={cn(Formstyles.Formfield, Zipstyles.postForm__address__group)}
+    >
+      <div style={{ display: "flex", flexDirection: "column" }}>
         <div>
-            <div><label>주소</label>{props.required && <span>*</span>}</div>
-            <div className={Zipstyles.zipCode__group}>
-                <input type="text" id="zipcode" value={zipcode} readOnly placeholder="01234" onChange={props.onChange} />
-                <Button variant='FormBtn' type='button' onClick={props.onClick}>우편번호 검색</Button>
-            </div>
+          <label>주소</label>
+          {props.required && <span>*</span>}
         </div>
-        <input type="text" id="address" placeholder={props.placeholder} value={address} readOnly onChange={props.onChange}/>
-        {props.placeholder_2 && <input type="text" id="addressDetail" placeholder={props.placeholder_2} value={addressDetail} onChange={props.onChange}/>}
+        <div className={Zipstyles.zipCode__group}>
+          <input
+            type="text"
+            id="zipcode"
+            value={zipcode}
+            readOnly
+            placeholder="01234"
+            onChange={props.onChange}
+          />
+          <Button variant="FormBtn" type="button" onClick={props.onClick}>
+            우편번호 검색
+          </Button>
+        </div>
       </div>
-    );
+      <input
+        type="text"
+        id="address"
+        placeholder={props.placeholder}
+        value={address}
+        readOnly
+        onChange={props.onChange}
+      />
+      {props.placeholder_2 && (
+        <input
+          type="text"
+          id="addressDetail"
+          placeholder={props.placeholder_2}
+          value={addressDetail}
+          onChange={props.onChange}
+        />
+      )}
+    </div>
+  );
 }
