017567c0e1bcb573c05ca27cab3ae8863b3e1ca2
szeunkk <szeunkk@gmail.com>
2025-10-01 00:18:08 +0900

    [트립트립] refactor: react-hook-form 사용 및 컴포넌트 재사용을 위한 inputfield 수정(forwardRef 사용)

--- maincamp/src/components/ui/input/Inputfield.tsx
+++ maincamp/src/components/ui/input/Inputfield.tsx
@@ -2,84 +2,98 @@ import styles from "./Inputfield.module.css";
 import { InputProps, TextareaProps } from "@/types/index";
 import { Modal } from "antd";
 import { useRouter } from "next/navigation";
+import { forwardRef } from "react";
 
 /* input 컴포넌트: 라벨, span, input */
-export function Inputfield(props: InputProps) {
-  const router = useRouter();
+export const Inputfield = forwardRef<HTMLInputElement, InputProps>(
+  (
+    { label, required, type, placeholder, id, isEdit, isAuth, ...rest },
+    ref
+  ) => {
+    const router = useRouter();
 
-  const handleUnauthClick = () => {
-    Modal.confirm({
-      title: "로그인 후 이용할 수 있습니다.",
-      content: "댓글 기능은 로그인 후 이용할 수 있습니다.",
-      okText: "로그인하기",
-      cancelText: "창닫기",
-      onOk() {
-        router.push("/login");
-      },
-      onCancel() {},
-    });
-  };
+    const handleUnauthClick = () => {
+      Modal.confirm({
+        title: "로그인 후 이용할 수 있습니다.",
+        content: "댓글 기능은 로그인 후 이용할 수 있습니다.",
+        okText: "로그인하기",
+        cancelText: "창닫기",
+        onOk() {
+          router.push("/login");
+        },
+        onCancel() {},
+      });
+    };
 
-  return (
-    <div className={styles.Formfield}>
-      <div>
-        <label>{props.label}</label>
-        {props.required && <span>*</span>}
+    return (
+      <div className={styles.Formfield}>
+        <div>
+          <label>{label}</label>
+          {required && <span>*</span>}
+        </div>
+        <input
+          type={type}
+          placeholder={placeholder}
+          readOnly={isAuth === false ? true : false}
+          disabled={isEdit}
+          onClick={isAuth === false ? handleUnauthClick : undefined}
+          {...rest}
+          ref={ref}
+        />
       </div>
-      <input
-        type={props.type}
-        placeholder={props.placeholder}
-        id={props.id}
-        value={
-          props.type === "password" && props.isEdit ? "*********" : props.value
-        }
-        readOnly={props.isAuth === false ? true : false}
-        disabled={props.isEdit}
-        onChange={props.onChange}
-        onClick={props.isAuth === false ? handleUnauthClick : undefined}
-      />
-    </div>
-  );
-}
+    );
+  }
+);
 
 /* TextArea 컴포넌트: 라벨, span, input */
-export function Textareafield(props: TextareaProps) {
-  const router = useRouter();
+export const Textareafield = forwardRef<HTMLTextAreaElement, TextareaProps>(
+  (
+    {
+      isCommentField,
+      label,
+      required,
+      placeholder,
+      id,
+      isAuth,
+      maxLength,
+      ...rest
+    },
+    ref
+  ) => {
+    const router = useRouter();
 
-  const handleUnauthClick = () => {
-    Modal.confirm({
-      title: "로그인 후 이용할 수 있습니다.",
-      content: "댓글 기능은 로그인 후 이용할 수 있습니다.",
-      okText: "로그인하기",
-      cancelText: "창닫기",
-      onOk() {
-        router.push("/login");
-      },
-      onCancel() {},
-    });
-  };
+    const handleUnauthClick = () => {
+      Modal.confirm({
+        title: "로그인 후 이용할 수 있습니다.",
+        content: "댓글 기능은 로그인 후 이용할 수 있습니다.",
+        okText: "로그인하기",
+        cancelText: "창닫기",
+        onOk() {
+          router.push("/login");
+        },
+        onCancel() {},
+      });
+    };
 
-  return (
-    <div
-      className={props.isCommentField ? styles.Commentfield : styles.Formfield}
-    >
-      {props.isCommentField ? (
-        ""
-      ) : (
-        <div>
-          <label>{props.label}</label>
-          {props.required && <span>*</span>}
-        </div>
-      )}
-      <textarea
-        placeholder={props.placeholder}
-        id={props.id}
-        value={props.value}
-        onChange={props.onChange}
-        readOnly={props.isAuth === false ? true : false}
-        maxLength={props.maxLength}
-        onClick={props.isAuth === false ? handleUnauthClick : undefined}
-      ></textarea>
-    </div>
-  );
-}
+    return (
+      <div className={isCommentField ? styles.Commentfield : styles.Formfield}>
+        {isCommentField ? (
+          ""
+        ) : (
+          <div>
+            <label>{label}</label>
+            {required && <span>*</span>}
+          </div>
+        )}
+        <textarea
+          placeholder={placeholder}
+          readOnly={isAuth === false ? true : false}
+          maxLength={maxLength}
+          onClick={isAuth === false ? handleUnauthClick : undefined}
+          {...rest}
+          ref={ref}
+        ></textarea>
+      </div>
+    );
+  }
+);
