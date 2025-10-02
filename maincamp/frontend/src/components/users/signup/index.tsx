392aa25954ea6b6df0bab796fc69953d1c056c6c
szeunkk <szeunkk@gmail.com>
2025-10-02 16:44:13 +0900

    [트립트립] refactor(signup): react-hook-form + zod 기반 회원가입 페이지 리팩토링

--- maincamp/frontend/src/components/users/signup/index.tsx
+++ maincamp/frontend/src/components/users/signup/index.tsx
@@ -4,60 +4,73 @@ import { Button, Inputfield } from "@commons/ui";
 import styles from "./styles.module.css";
 import useSignUp from "./hook";
 import { Modal } from "antd";
+import useSignupForm from "./useSignupForm";
+import classNames from "classnames";
 
 export default function Signup() {
-  const { errors, onChangeInputs, onClickSignup, onClickLogin, isModalOpen } = useSignUp();
+  // const { errors, onChangeInputs, onClickSignup, onClickLogin, isModalOpen } = useSignUp();
+
+  const { register, handleSubmit, formState, isModalOpen, onClickSignup, onClickLogin } = useSignupForm();
   return (
     <>
-      <form className={styles.formContainer} onSubmit={onClickSignup}>
+      <form className={styles.formContainer} onSubmit={handleSubmit(onClickSignup)}>
         <span className={styles.title}>회원가입</span>
         <div>
           회원가입을 위해 아래 빈칸을 모두 채워 주세요
           <div className={styles.inputContainer}>
-            <div className={errors.email ? styles.error : ""}>
+            <div className={classNames({ [styles.error]: formState.isSubmitted && formState.errors.email })}>
               <Inputfield
                 type="text"
                 placeholder="이메일을 입력해 주세요."
                 label="이메일"
                 required
-                id="email"
-                onChange={onChangeInputs}
+                {...register("email")}
+                // error={formState.errors.email?.message}
               />
-              {errors.email && <span className={styles.errorMessage}>이메일을 입력해 주세요.</span>}
+              {formState.isSubmitted && formState.errors.email && (
+                <span className={styles.errorMessage}>{formState.errors.email.message}</span>
+              )}
             </div>
-            <div className={errors.name ? styles.error : ""}>
+            <div className={classNames({ [styles.error]: formState.isSubmitted && formState.errors.name })}>
               <Inputfield
                 type="text"
                 placeholder="이름을 입력해 주세요."
                 label="이름"
                 required
-                id="name"
-                onChange={onChangeInputs}
+                {...register("name")}
               />
-              {errors.name && <span className={styles.errorMessage}>이름을 입력해 주세요.</span>}
+              {formState.isSubmitted && formState.errors.name && (
+                <span className={styles.errorMessage}>{formState.errors.name.message}</span>
+              )}
             </div>
-            <div className={errors.password ? styles.error : ""}>
+            <div
+              className={classNames({ [styles.error]: formState.isSubmitted && formState.errors.password })}
+            >
               <Inputfield
                 type="password"
                 placeholder="비밀번호를 입력해 주세요."
                 label="비밀번호"
                 required
-                id="password"
-                onChange={onChangeInputs}
+                {...register("password")}
               />
-              {errors.password && <span className={styles.errorMessage}>비밀번호를 입력해 주세요.</span>}
+              {formState.isSubmitted && formState.errors.password && (
+                <span className={styles.errorMessage}>{formState.errors.password.message}</span>
+              )}
             </div>
-            <div className={errors.passwordConfirm ? styles.error : ""}>
+            <div
+              className={classNames({
+                [styles.error]: formState.isSubmitted && formState.errors.passwordConfirm,
+              })}
+            >
               <Inputfield
                 type="password"
                 placeholder="비밀번호를 한번 더 입력해 주세요."
                 label="비밀번호 확인"
                 required
-                id="passwordConfirm"
-                onChange={onChangeInputs}
+                {...register("passwordConfirm")}
               />
-              {errors.passwordConfirm && (
-                <span className={styles.errorMessage}>비밀번호를 입력해 주세요.</span>
+              {formState.isSubmitted && formState.errors.passwordConfirm && (
+                <span className={styles.errorMessage}>{formState.errors.passwordConfirm.message}</span>
               )}
             </div>
           </div>
