fad71e22702a69e38c0dca6cdfc827094ba7d9a4
szeunkk <szeunkk@gmail.com>
2025-11-05 16:00:04 +0900

    feat: 숙박권 상세 페이지 및 카드 컴포넌트 추가

--- maincamp/frontend/src/app/products/[productId]/page.tsx
+++ maincamp/frontend/src/app/products/[productId]/page.tsx
@@ -1,10 +1,10 @@
 import ProductsDetailContents from "@/components/features/products/products-detail/contents";
+import styles from "./styles.module.css";
 
 export default function ProductDetailPage() {
   return (
-    <>
+    <div className={styles.productsDetail}>
       <ProductsDetailContents />
-    </>
+    </div>
   );
 }
-
