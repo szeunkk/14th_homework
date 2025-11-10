e68a8c5bc7d24afe6482498785000d688f716b1a
szeunkk <szeunkk@gmail.com>
2025-11-10 21:38:14 +0900

    feat: 포인트 충전 모달 컴포넌트 구현
    - Profile 컴포넌트에서 포인트 충전 탭 클릭 시 모달 표시
    - React Portal을 사용한 모달 렌더링
    - Dropdown으로 충전 금액 선택 (100, 500, 2,000, 5,000, 10,000, 50,000)
    - 모달 열림 시 배경 스크롤 방지
    - Figma 디자인 기반 UI 구현
    - globals.css 색상/타이포그래피 토큰 활용

--- maincamp/frontend/src/commons/layout/navigation/hook.ts
+++ maincamp/frontend/src/commons/layout/navigation/hook.ts
@@ -1,6 +1,7 @@
 import { FETCH_USER_LOGGED_IN } from "@/graphql/queries/login";
 import { useQuery } from "@apollo/client";
 import { usePathname, useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function useNavigation() {
   const router = useRouter();
@@ -8,6 +9,10 @@ export default function useNavigation() {
   const pathname = usePathname();
 
   const { data } = useQuery(FETCH_USER_LOGGED_IN);
+
+  const [isProfileOpen, setIsProfileOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   const onClickBoards = () => {
     router.push("/boards");
   };
@@ -23,5 +28,42 @@ export default function useNavigation() {
     router.push("/products");
   };
 
-  return { onClickBoards, onClickLogin, onClickMypage, onClickProducts, data, pathname };
+  const onClickProfile = () => {
+    setIsProfileOpen((prev) => !prev);
+  };
+
+  const onLogout = () => {
+    // TODO: 로그아웃 로직 구현
+    console.log("로그아웃");
+  };
+
+  const onPointCharge = () => {
+    setIsModalOpen(true);
+    setIsProfileOpen(false);
+  };
+
+  const onModalClose = () => {
+    setIsModalOpen(false);
+  };
+
+  const onModalSubmit = (amount: number) => {
+    // TODO: 포인트 충전 로직 구현
+    console.log("포인트 충전:", amount);
+  };
+
+  return {
+    onClickBoards,
+    onClickLogin,
+    onClickMypage,
+    onClickProducts,
+    onClickProfile,
+    onLogout,
+    onPointCharge,
+    onModalClose,
+    onModalSubmit,
+    data,
+    pathname,
+    isProfileOpen,
+    isModalOpen,
+  };
 }
